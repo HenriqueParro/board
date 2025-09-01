acc166409857b3d4126c60e7a95f5669db2fbc17
Henrique Parro <59770362+HenriqueParro@users.noreply.github.com>
2025-08-31 22:23:05 -0300

    Update CardEntity.java

--- src/main/java/br/com/dio/persistence/entity/CardEntity.java
+++ src/main/java/br/com/dio/persistence/entity/CardEntity.java
@@ -2,12 +2,18 @@ package br.com.dio.persistence.entity;
 
 import lombok.Data;
 
+import java.time.OffsetDateTime;
+
 @Data
 public class CardEntity {
 
     private Long id;
     private String title;
     private String description;
+
+    // coluna atual onde o card está
     private BoardColumnEntity boardColumn = new BoardColumnEntity();
 
+    // novo: data/hora de criação do card (preenchido pelo BD via DEFAULT CURRENT_TIMESTAMP)
+    private OffsetDateTime createdAt;
 }
